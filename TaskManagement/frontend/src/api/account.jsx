4abab9f65f249a60a98902e858f08f67cc5aa1a9
h76ziabakhsh <h76ziabakhsh@yahoo.com>
2026-05-07 14:33:06 -0700

    cleaned files

--- TaskManagement/frontend/src/api/account.jsx
+++ TaskManagement/frontend/src/api/account.jsx
@@ -1,9 +1,4 @@
-import axios from "axios";
-import { api } from "../api/axios";
-
-// const API = 'https://localhost:7139/api/acccount'
-
-// import { api } from "./axios";
+import api from "./api";
 
 export const registerUser = async (data) => {
   const res = await api.post("/account/register", data);
@@ -14,9 +9,3 @@ export const loginUser = async (data) => {
   const res = await api.post("/account/login", data);
   return res.data;
 };
-
-// export const loginUser = async (userData) => {
-//   const response = await api.post("/account/login", userData);
-
-//   return response.data;
-// };
--- TaskManagement/frontend/src/pages/Tasks.jsx
+++ TaskManagement/frontend/src/pages/Tasks.jsx
@@ -2,37 +2,52 @@ import { useTasks } from "../hooks/useTasks";
 import TaskForm from "../components/TaskForm";
 
 export default function Tasks() {
-  const { data, isLoading, error, deleteTask, updateTask } = useTasks();
+    const { data, isLoading, error, deleteTask, updateTask } = useTasks();
 
-  if (isLoading) return <p>Loading...</p>;
-  if (error) return <p>Error loading tasks</p>;
+    if (isLoading) return <p>Loading...</p>;
+    if (error) return <p>Error loading tasks</p>;
 
-  return (
-    <div>
-      <h2>Tasks</h2>
+    return (
+        <div>
+            <h2>Tasks</h2>
 
-      {data.map((task) => (
-        <div key={task.id}>
-          <span
-            style={{
-              textDecoration: task.completed ? "line-through" : "none",
-            }}
-          >
-            {task.title}
-          </span>
+            {data.map((task) => (
+                <div key={task.id}
+                    style={{
+                        border: "1px solid #ccc",
+                        borderRadius: "8px",
+                        padding: "16px",
+                        display: "grid",
+                        gap: "10px",
+                        backgroundColor: "#f9f9f9",
+                    }}>
+                    <span
+                        style={{
+                            textDecoration: task.completed ? "line-through" : "none",
+                        }}
+                    >
+                        {task.title}
+                    </span>
+                    <span
+                        style={{
+                            textDecoration: task.completed ? "line-through" : "none",
+                        }}
+                    >
+                        {task.description}
+                    </span>
+                    <button onClick={() => deleteTask(task.id)}>Delete</button>
 
-          <button onClick={() => deleteTask(task.id)}>Delete</button>
-
-          <button
-            onClick={() =>
-              updateTask({ ...task, completed: !task.completed })
-            }
-          >
-            Toggle
-          </button>
+                    <button
+                        onClick={() =>
+                            updateTask({ ...task, completed: !task.completed })
+                        }
+                    >
+                        Toggle
+                    </button>
+                </div>
+            ))}
+            <TaskForm />
         </div>
-      ))}
-      <TaskForm />
-    </div>
-  );
+    );
 }
+
